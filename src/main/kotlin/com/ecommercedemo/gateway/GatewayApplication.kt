d04447d3dfcbda6a6caab24ea1d5a17e6baf4742
aron <vaupelaron@gmail.com>
2024-10-21 16:48:54 +0700

    Explicitly add args to docker-compose.yml and add component scan

--- src/main/kotlin/com/ecommercedemo/gateway/GatewayApplication.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/GatewayApplication.kt
@@ -2,8 +2,10 @@ package com.ecommercedemo.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("com.ecommercedemo.gateway", "com.ecommercedemo.common")
 class GatewayApplication
 
 fun main(args: Array<String>) {
