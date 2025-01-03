35aa930dc0f2b2229716aea48e73d150c09acefc
aron <vaupelaron@gmail.com>
2025-01-04 01:53:44 +0700

    Add ObjectMapper configuration with module auto-registration
    Introduced a new configuration class for ObjectMapper to ensure automatic module registration using Spring's @Bean and @Configuration annotations. This simplifies JSON handling and integrates seamlessly with custom serializers or deserializers.

---
+++ src/main/kotlin/com/ecommercedemo/gateway/config/jackson/ObjectMapperConfig.kt
@@ -0,0 +1,15 @@
+package com.ecommercedemo.gateway.config.jackson
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ObjectMapperConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper()
+            .findAndRegisterModules()
+    }
+}
