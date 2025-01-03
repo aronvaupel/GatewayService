878635d621dec3ca866e34258f45c61632ef79ad
aron <vaupelaron@gmail.com>
2025-01-04 00:57:14 +0700

    Refactor UserActivityTrackerService to use StringRedisTemplate
    Switched from RedisTemplate to StringRedisTemplate for better compatibility with string-based operations. Updated type handling for Redis values to ensure proper conversion between strings and timestamps. This improves the clarity and maintainability of the Redis interactions.

--- src/main/kotlin/com/ecommercedemo/gateway/service/UserActivityTrackerService.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/service/UserActivityTrackerService.kt
@@ -2,6 +2,7 @@ package com.ecommercedemo.gateway.service
 
 import org.springframework.context.annotation.DependsOn
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import java.time.Instant
 import java.time.LocalDateTime
@@ -10,18 +11,17 @@ import java.util.*
 
 
 @Service
-@DependsOn("redisTemplate")
 class UserActivityTrackerService(
-    private val redisTemplate: RedisTemplate<String, Any>
+    private val redisTemplate: StringRedisTemplate
 ) {
 
     fun updateLastActivity(userId: UUID, lastActivityTimestamp: Long) {
         val lastActivity = LocalDateTime.ofInstant(Instant.ofEpochMilli(lastActivityTimestamp), ZoneId.systemDefault())
-        redisTemplate.opsForValue().set("user:lastActivity:$userId", lastActivityTimestamp)
+        redisTemplate.opsForValue().set("user:lastActivity:$userId", lastActivityTimestamp.toString())
     }
 
     fun getLastActivity(userId: UUID): LocalDateTime? {
-        val lastActivityTimestamp = redisTemplate.opsForValue().get("user:lastActivity:$userId") as Long?
+        val lastActivityTimestamp = redisTemplate.opsForValue().get("user:lastActivity:$userId")?.toLong()
         return lastActivityTimestamp?.let {
             LocalDateTime.ofInstant(Instant.ofEpochMilli(it), ZoneId.systemDefault())
         }
