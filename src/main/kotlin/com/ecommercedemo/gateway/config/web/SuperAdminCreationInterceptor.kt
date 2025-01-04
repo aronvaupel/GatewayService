9e75a3cf311b0aec9dff260f2e9ae928944367a2
aron <vaupelaron@gmail.com>
2025-01-05 00:57:54 +0700

    Disable SuperAdmin creation logic in interceptor.
    Commented out the `preHandle` method and related logic for handling SuperAdmin creation checks in `SuperAdminCreationInterceptor`. These changes are likely temporary and might indicate a need to revisit the functionality or workflow.

--- src/main/kotlin/com/ecommercedemo/gateway/config/web/SuperAdminCreationInterceptor.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/config/web/SuperAdminCreationInterceptor.kt
@@ -15,39 +15,39 @@ class SuperAdminCreationInterceptor(
     private val superAdminCounter: SuperAdminCounter
 ) : HandlerInterceptor {
 
-    override fun preHandle(
-        request: HttpServletRequest,
-        response: HttpServletResponse,
-        handler: Any
-    ): Boolean {
-        val createRequest = try {
-            val requestBody = request.inputStream.bufferedReader().use { it.readText() }
-            ObjectMapper().readValue(requestBody, CreateRequest::class.java)
-        } catch (e: Exception) {
-            return true
-        }
-
-        if (createRequest.entityClassName != "User") return true
-        else {
-            if (createRequest.properties["userRole"] == "SUPER_ADMIN") {
-                val authentication = SecurityContextHolder.getContext().authentication
-                val creatorRole = extractRoleFromToken(authentication.credentials.toString())
-                val superAdminCount = superAdminCounter.getSuperAdminCount()
-
-                when {
-                    superAdminCount == 0 -> return true
-                    superAdminCount >= 3 -> return false
-                    superAdminCount in 1..2 && creatorRole != "SUPER_ADMIN" -> return false
-                }
-            }
-        }
-
-        return true
-    }
-
-    private fun extractRoleFromToken(token: String): String {
-        val claims = JwtUtil.getClaimsFromToken(token)
-        return claims["role"] as? String
-            ?: throw AccessDeniedException("Unauthorized: Role claim is missing in token")
-    }
+//    override fun preHandle(
+//        request: HttpServletRequest,
+//        response: HttpServletResponse,
+//        handler: Any
+//    ): Boolean {
+//        val createRequest = try {
+//            val requestBody = request.inputStream.bufferedReader().use { it.readText() }
+//            ObjectMapper().readValue(requestBody, CreateRequest::class.java)
+//        } catch (e: Exception) {
+//            return true
+//        }
+//
+//        if (createRequest.entityClassName != "User") return true
+//        else {
+//            if (createRequest.properties["userRole"] == "SUPER_ADMIN") {
+//                val authentication = SecurityContextHolder.getContext().authentication
+//                val creatorRole = extractRoleFromToken(authentication.credentials.toString())
+//                val superAdminCount = superAdminCounter.getSuperAdminCount()
+//
+//                when {
+//                    superAdminCount == 0 -> return true
+//                    superAdminCount >= 3 -> return false
+//                    superAdminCount in 1..2 && creatorRole != "SUPER_ADMIN" -> return false
+//                }
+//            }
+//        }
+//
+//        return true
+//    }
+//
+//    private fun extractRoleFromToken(token: String): String {
+//        val claims = JwtUtil.getClaimsFromToken(token)
+//        return claims["role"] as? String
+//            ?: throw AccessDeniedException("Unauthorized: Role claim is missing in token")
+//    }
 }
