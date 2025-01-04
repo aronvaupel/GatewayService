ed4be02ef3668ae08cacfbbc420556699dd34b91
aron <vaupelaron@gmail.com>
2025-01-04 23:00:44 +0700

    Refactor GitHub credentials retrieval logic.
    Simplified environment variable handling by removing the redundant `env` variable and directly using `loadEnv()` for GitHub credentials. This improves readability and reduces unnecessary variable usage.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,8 @@ fun loadEnv(): Map<String, String> {
 		.associate { it[0] to it.getOrElse(1) { "" } }
 }
 
-val env = loadEnv()
-
-val githubUsername: String? = project.findProperty("githubUsername") as String? ?: System.getenv("GITHUB_USERNAME") ?: env["GITHUB_USERNAME"]
-val githubToken: String? = project.findProperty("githubToken") as String? ?: System.getenv("GITHUB_TOKEN") ?: env["GITHUB_TOKEN"]
+val githubUsername: String? = project.findProperty("githubUsername") as String? ?: System.getenv("GITHUB_USERNAME") ?: loadEnv()["GITHUB_USERNAME"]
+val githubToken: String? = project.findProperty("githubToken") as String? ?: System.getenv("GITHUB_TOKEN") ?: loadEnv()["GITHUB_TOKEN"]
 
 extra["springCloudVersion"] = "2023.0.3"
 
