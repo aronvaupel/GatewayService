ba9eb007f2a49f34ce61eed96f489702739c9c9b
aron <vaupelaron@gmail.com>
2025-01-05 13:43:10 +0700

    Update dependency and configure JPA settings in Gateway app
    Upgraded the commons library to version 6.5.2 in `build.gradle.kts` to incorporate the latest changes. Added `@EntityScan` and `@EnableJpaRepositories` annotations in `GatewayApplication.kt` for proper JPA entity and repository configuration. This ensures compatibility and improved functionality within the application.

--- src/main/kotlin/com/ecommercedemo/gateway/GatewayApplication.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/GatewayApplication.kt
@@ -1,11 +1,15 @@
 package com.ecommercedemo.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @ComponentScan("com.ecommercedemo.gateway", "com.ecommercedemo.common")
+@EntityScan(basePackages = ["com.ecommercedemo.gateway"])
+@EnableJpaRepositories(basePackages = ["com.ecommercedemo.gateway.persistence", "com.ecommercedemo.common.persistence"])
 class GatewayApplication
 
 fun main(args: Array<String>) {
