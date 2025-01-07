2eb97e792f3b3b5d3163227313952284ed972c0d
aron <vaupelaron@gmail.com>
2025-01-07 22:27:09 +0700

    Add base class to _PermissionRestService
    Integrates _PermissionRestService with DownstreamRestServiceTemplate to provide reusable logic for downstream REST operations. This change ensures consistency and reduces duplication across similar services.

--- src/main/kotlin/com/ecommercedemo/gateway/service/_PermissionRestService.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/service/_PermissionRestService.kt
@@ -1,11 +1,11 @@
 package com.ecommercedemo.gateway.service
 
 import com.ecommercedemo.common.model.concretion.permission.Permission
+import com.ecommercedemo.common.service.abstraction.DownstreamRestServiceTemplate
 import com.ecommercedemo.common.service.annotation.RestServiceFor
 import org.springframework.stereotype.Service
 
 @Service
 @Suppress("ClassName")
 @RestServiceFor(Permission::class)
-class _PermissionRestService {
-}
+class _PermissionRestService : DownstreamRestServiceTemplate<Permission>()
