291ca1be9597db105af1d2fb2a7afbc5e364c514
aron <vaupelaron@gmail.com>
2025-01-07 20:56:24 +0700

    Update dependencies, enhance component scanning, and cleanup config
    Upgraded `commons` library to version 6.6.3 for improved functionality. Extended component scanning exclusions to handle additional patterns in `GatewayApplication`. Removed unused `pseudo-properties` configuration from `application.yml` for better clarity.

--- src/main/kotlin/com/ecommercedemo/gateway/GatewayApplication.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/GatewayApplication.kt
@@ -9,23 +9,22 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @ComponentScan(
-	basePackages = ["com.ecommercedemo.gateway", "com.ecommercedemo.common"],
-	excludeFilters = [ComponentScan.Filter(
-		type = FilterType.REGEX,
-		pattern = ["com\\.ecommercedemo\\.common\\.model\\.concretion\\._pseudoProperty\\..*"]
-	)]
+    basePackages = ["com.ecommercedemo.gateway", "com.ecommercedemo.common"], excludeFilters = [ComponentScan.Filter(
+        type = FilterType.REGEX,
+        pattern = ["com\\.ecommercedemo\\.common\\.model\\.concretion\\._pseudoProperty\\..*",
+            "com\\.ecommercedemo\\.common\\.model\\.concretion\\.permission\\..*",
+            "com\\.ecommercedemo\\.common\\.model\\.concretion\\.permissionuserassociation\\..*"
+        ]
+    )]
 )
-
 @EntityScan("com.ecommercedemo.gateway")
 @EnableJpaRepositories(
-	basePackages = ["com.ecommercedemo.gateway.persistence"],
-	excludeFilters = [ComponentScan.Filter(
-		type = FilterType.REGEX,
-		pattern = ["com\\.ecommercedemo\\.common\\.persistence\\..*"]
-	)]
+    basePackages = ["com.ecommercedemo.gateway.persistence"], excludeFilters = [ComponentScan.Filter(
+        type = FilterType.REGEX, pattern = ["com\\.ecommercedemo\\.common\\.persistence\\..*"]
+    )]
 )
 class GatewayApplication
 
 fun main(args: Array<String>) {
-	runApplication<GatewayApplication>(*args)
+    runApplication<GatewayApplication>(*args)
 }
