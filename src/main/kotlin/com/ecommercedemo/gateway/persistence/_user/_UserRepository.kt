8455d4f65bbc94d6cc1c153163bffc80781cfe6a
aron <vaupelaron@gmail.com>
2025-01-07 22:39:05 +0700

    Update import for UUID in _UserRepository interface
    Replaced org.hibernate.validator.constraints.UUID with java.util.UUID. This ensures compatibility and aligns with standard library usage. No functional changes were introduced.

--- src/main/kotlin/com/ecommercedemo/gateway/persistence/_user/_UserRepository.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/persistence/_user/_UserRepository.kt
@@ -2,7 +2,8 @@ package com.ecommercedemo.gateway.persistence._user
 
 import com.ecommercedemo.common.persistence.abstraction.EntityRepository
 import com.ecommercedemo.gateway.model._user._User
-import org.hibernate.validator.constraints.UUID
+import java.util.*
+
 
 @Suppress("ClassName", "unused")
 interface _UserRepository : EntityRepository<_User, UUID>
