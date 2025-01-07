ff765c016a3a40b3f72a4e70bfb58aa114683665
aron <vaupelaron@gmail.com>
2025-01-08 02:01:50 +0700

    Update commons library to version 6.6.7
    Upgraded the dependency `com.github.aronvaupel:commons` from version 6.6.6 to 6.6.7. This ensures the project benefits from the latest fixes and improvements in the library.

--- src/main/kotlin/com/ecommercedemo/gateway/service/_PermissionEventService.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/service/_PermissionEventService.kt
@@ -1,11 +1,11 @@
 package com.ecommercedemo.gateway.service
 
-import com.ecommercedemo.common.model.concretion.permission.Permission
 import com.ecommercedemo.common.service.abstraction.EventServiceTemplate
 import com.ecommercedemo.common.service.annotation.EventServiceFor
+import com.ecommercedemo.gateway.model._permission._Permission
 import org.springframework.stereotype.Service
 
 @Suppress("ClassName")
 @Service
-@EventServiceFor(Permission::class)
-class _PermissionEventService : EventServiceTemplate<Permission>()
+@EventServiceFor(_Permission::class)
+class _PermissionEventService : EventServiceTemplate<_Permission>()
--- src/main/kotlin/com/ecommercedemo/gateway/service/_PermissionRestService.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/service/_PermissionRestService.kt
@@ -1,11 +1,11 @@
 package com.ecommercedemo.gateway.service
 
-import com.ecommercedemo.common.model.concretion.permission.Permission
 import com.ecommercedemo.common.service.abstraction.DownstreamRestServiceTemplate
 import com.ecommercedemo.common.service.annotation.RestServiceFor
+import com.ecommercedemo.gateway.model._permission._Permission
 import org.springframework.stereotype.Service
 
 @Service
 @Suppress("ClassName")
-@RestServiceFor(Permission::class)
-class _PermissionRestService : DownstreamRestServiceTemplate<Permission>()
+@RestServiceFor(_Permission::class)
+class _PermissionRestService : DownstreamRestServiceTemplate<_Permission>()
