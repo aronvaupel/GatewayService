f6a1a61ad1a20bd9c5a175823b78f76128ecbcca
aron <vaupelaron@gmail.com>
2025-01-09 17:01:10 +0700

    Add @Service annotation to _UserCreateHandler class
    This change marks the _UserCreateHandler class as a Spring service, ensuring it is properly registered as a Spring bean. This is necessary for dependency injection and better integration with the Spring framework.

--- src/main/kotlin/com/ecommercedemo/gateway/kafka/_UserCreateHandler.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/kafka/_UserCreateHandler.kt
@@ -4,8 +4,10 @@ import com.ecommercedemo.common.application.kafka.EntityEvent
 import com.ecommercedemo.common.application.kafka.handling.abstraction.ICreateHandler
 import com.ecommercedemo.gateway.model._user._User
 import com.ecommercedemo.gateway.service._UserEventService
+import org.springframework.stereotype.Service
 
 @Suppress("ClassName", "unused")
+@Service
 class _UserCreateHandler(
     private val _userEventService: _UserEventService
 ) : ICreateHandler<_User> {
