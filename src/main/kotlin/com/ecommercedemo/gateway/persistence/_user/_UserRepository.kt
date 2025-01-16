e729929fdeb0437a201df9cb6ad01e36adc3c533
aron <vaupelaron@gmail.com>
2025-01-16 23:53:53 +0700

    attempt to resolve _password field in repo

--- src/main/kotlin/com/ecommercedemo/gateway/persistence/_user/_UserRepository.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/persistence/_user/_UserRepository.kt
@@ -3,11 +3,14 @@ package com.ecommercedemo.gateway.persistence._user
 import com.ecommercedemo.common.application.validation.userrole.UserRole
 import com.ecommercedemo.common.persistence.abstraction.EntityRepository
 import com.ecommercedemo.gateway.model._user._User
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.*
 
 
 @Suppress("ClassName", "unused")
 interface _UserRepository : EntityRepository<_User, UUID> {
-    fun findByUsernameAndPassword(username: String, hashedPassword: String): _User?
+    @Query("SELECT u FROM _User u WHERE u.username = :username AND u._password = :hashedPassword")
+    fun findByUsernameAndPassword(@Param("username") username: String, @Param("hashedPassword") hashedPassword: String): _User?
     fun countByUserRoleIs(userRole: UserRole): Int
 }
