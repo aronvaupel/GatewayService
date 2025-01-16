d4c8fe7480ed1fa5aa80d971ab2f35afc227a1bf
aron <vaupelaron@gmail.com>
2025-01-17 02:25:50 +0700

    update commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ repositories {
 extra["springCloudVersion"] = "2023.0.3"
 
 dependencies {
-	implementation("com.github.aronvaupel:commons:7.2.4")
+	implementation("com.github.aronvaupel:commons:7.2.5")
 	implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.0")
 	implementation("org.hibernate:hibernate-core:6.6.3.Final")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
