e3e3b9c7da13b9b19bf5fee7cd87f2a583ce7d44
aron <vaupelaron@gmail.com>
2025-01-17 17:38:54 +0700

    change jwt lib

--- src/main/kotlin/com/ecommercedemo/gateway/config/security/JwtUtil.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/config/security/JwtUtil.kt
@@ -4,14 +4,20 @@ import com.ecommercedemo.common.application.validation.userrole.UserRole
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.*
+import javax.crypto.SecretKey
 
 @Component
 class JwtUtil {
     @Value("\${security.jwt.secret}")
     private lateinit var jwtSecret: String
+
+    private val key: SecretKey by lazy {
+        Keys.hmacShaKeyFor(jwtSecret.toByteArray())
+    }
 
     fun generateToken(
         username: String,
@@ -32,7 +38,7 @@ class JwtUtil {
 
     fun validateToken(token: String): Boolean {
         return try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token)
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
             true
         } catch (e: Exception) {
             false
@@ -40,15 +46,16 @@ class JwtUtil {
     }
 
     fun getUsernameFromToken(token: String): String {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).body.subject
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).body.subject
     }
 
     fun getRoleFromToken(token: String) = getClaimsFromToken(token)["role"] as String
 
 
     fun getClaimsFromToken(token: String): Claims {
-        return Jwts.parser()
-            .setSigningKey(jwtSecret)
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
             .parseClaimsJws(token)
             .body
     }
