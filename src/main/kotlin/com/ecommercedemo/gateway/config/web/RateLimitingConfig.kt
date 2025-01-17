2c0b5dd530b8b6fbe09f13a02c82e96411f78be8
aron <vaupelaron@gmail.com>
2025-01-17 22:55:55 +0700

    make RateLimitingConfig use JwtUtil

--- src/main/kotlin/com/ecommercedemo/gateway/config/web/RateLimitingConfig.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/config/web/RateLimitingConfig.kt
@@ -1,12 +1,10 @@
 package com.ecommercedemo.gateway.config.web
 
 import com.ecommercedemo.common.application.validation.userrole.UserRole
-import io.github.cdimascio.dotenv.Dotenv
-import io.jsonwebtoken.Jwts
+import com.ecommercedemo.gateway.config.security.JwtUtil
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.core.RedisTemplate
@@ -18,18 +16,16 @@ import java.util.concurrent.TimeUnit
 @Configuration
 class RateLimitingConfig @Autowired constructor(
     private val rateLimitResolver: RateLimitResolver,
-    private val redisTemplate: RedisTemplate<String, String>
+    private val redisTemplate: RedisTemplate<String, String>,
+    private val jwtUtil: JwtUtil,
 ) {
-
-    @Value("\${security.jwt.secret}")
-    private lateinit var jwtSecret: String
 
     @Bean
     fun rateLimitingInterceptor(): HandlerInterceptor {
         return object : HandlerInterceptor {
             override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
                 val token = request.getHeader("Authorization")?.substringAfter("Bearer ")
-                val role = token?.let { extractRoleFromToken(it) } ?: "guest"
+                val role = token?.let { jwtUtil.getRoleFromToken(it) } ?: "guest"
                 val (replenishRate, burstCapacity) = rateLimitResolver.resolveRateLimiter(
                     UserRole.valueOf(
                         role.uppercase(
@@ -52,11 +48,4 @@ class RateLimitingConfig @Autowired constructor(
         }
     }
 
-    private fun extractRoleFromToken(token: String): String {
-        val claims = Jwts.parser()
-            .setSigningKey(jwtSecret)
-            .parseClaimsJws(token)
-            .body
-        return claims["role"] as String
-    }
 }
