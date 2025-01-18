95cc01af7d5af6cf9b3b3c25054cce839ca39bdf
aron <vaupelaron@gmail.com>
2025-01-18 21:13:17 +0700

    fix pw column

--- src/main/kotlin/com/ecommercedemo/gateway/model/_user/_User.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/model/_user/_User.kt
@@ -20,7 +20,6 @@ open class _User(
 
     @ValidPassword
     @NotBlank(message = "Password is mandatory")
-    @Column(name = "password", nullable = false)
     private var _password: String = "",
 
     @Enumerated(EnumType.ORDINAL)
