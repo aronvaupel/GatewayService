6b3a70f9abbb9d87f3be6b54b36df1829f9d8b1b
aron <vaupelaron@gmail.com>
2025-01-19 20:01:04 +0700

    add missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.2")
+	implementation("org.webjars:webjars-locator-core")
+	implementation("org.webjars:swagger-ui")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.1.3")
