b2073cacb1461bc18ae9c71acda9f5d283aa00ec
aron <vaupelaron@gmail.com>
2025-01-19 22:44:30 +0700

    allow also subsequent path segments for error

--- src/main/kotlin/com/ecommercedemo/gateway/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/config/security/SecurityConfig.kt
@@ -29,7 +29,7 @@ class SecurityConfig(private val jwtRequestFilter: JwtRequestFilter) {
                         "/webjars/**",
                         "/actuator/health",
                         "/favicon.ico",
-                        "/error"
+                        "/error/**"
                     ).permitAll()
                     .anyRequest().authenticated()
             }
