45e9c22966b3e876f057dc11bc5b3262ecb71cc0
aron <vaupelaron@gmail.com>
2025-01-20 02:29:19 +0700

    try to redirect swagger in routRequest

--- src/main/kotlin/com/ecommercedemo/gateway/controller/GatewayController.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/controller/GatewayController.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,11 +14,6 @@ import org.springframework.web.bind.annotation.RestController
 class GatewayController(
     private val requestForwardingService: RequestForwardingService
 ) {
-    @Operation(summary = "Serve Swagger UI directly.")
-    @GetMapping("/swagger-ui/**")
-    fun serveSwaggerUI(request: HttpServletRequest, response: HttpServletResponse) {
-        request.getRequestDispatcher(request.requestURI).forward(request, response)
-    }
 
     @Operation(summary = "Route requests to downstream services.")
     @RequestMapping("/{serviceName}/**")
@@ -28,6 +22,10 @@ class GatewayController(
         request: HttpServletRequest,
         response: HttpServletResponse
     ) {
+        if (serviceName == "swagger-ui") {
+            response.sendRedirect(request.requestURI)
+            return
+        }
         requestForwardingService.forwardRequest(request, response, serviceName)
     }
 
