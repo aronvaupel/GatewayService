b8b196205f87bcbb7dbb5946ab055b305356b9d6
aron <vaupelaron@gmail.com>
2025-01-20 21:00:22 +0700

    add logging

--- src/main/kotlin/com/ecommercedemo/gateway/config/swagger/SwaggerAggregatorConfig.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/config/swagger/SwaggerAggregatorConfig.kt
@@ -13,8 +13,10 @@ class SwaggerAggregatorConfig(
     @PostConstruct
     fun configureSwagger() {
         val services = discoveryClient.services
+        println("SWAGGER DISCOVERED SERVICES: $services")
         services.forEach { serviceName ->
             val instances = discoveryClient.getInstances(serviceName)
+            println("SWAGGER DISCOVERED INSTANCES: $serviceName, INSTANCES: $instances")
             instances.forEach { instance ->
                 val openApiUrl = instance.metadata["openapi-docs"]
                 println("SERVICE: $serviceName, OPENAPI URL: $openApiUrl")
