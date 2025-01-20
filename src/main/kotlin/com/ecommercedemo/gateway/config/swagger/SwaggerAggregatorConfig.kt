40258c515286ef30e63a002e42c03afa1b1027dd
aron <vaupelaron@gmail.com>
2025-01-20 21:54:30 +0700

    try without aggregation

--- src/main/kotlin/com/ecommercedemo/gateway/config/swagger/SwaggerAggregatorConfig.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/config/swagger/SwaggerAggregatorConfig.kt
@@ -10,20 +10,20 @@ class SwaggerAggregatorConfig(
     private val discoveryClient: org.springframework.cloud.client.discovery.DiscoveryClient
 ) {
 
-    @PostConstruct
-    fun configureSwagger() {
-        val services = discoveryClient.services
-        println("SWAGGER DISCOVERED SERVICES: $services")
-        services.forEach { serviceName ->
-            val instances = discoveryClient.getInstances(serviceName)
-            println("SWAGGER DISCOVERED INSTANCES: $serviceName, INSTANCES: $instances")
-            instances.forEach { instance ->
-                val openApiUrl = instance.metadata["openapi-docs"]
-                println("SERVICE: $serviceName, OPENAPI URL: $openApiUrl")
-                if (!openApiUrl.isNullOrBlank()) {
-                    swaggerUiConfigParameters.addGroup(serviceName, openApiUrl)
-                }
-            }
-        }
-    }
+//    @PostConstruct
+//    fun configureSwagger() {
+//        val services = discoveryClient.services
+//        println("SWAGGER DISCOVERED SERVICES: $services")
+//        services.forEach { serviceName ->
+//            val instances = discoveryClient.getInstances(serviceName)
+//            println("SWAGGER DISCOVERED INSTANCES: $serviceName, INSTANCES: $instances")
+//            instances.forEach { instance ->
+//                val openApiUrl = instance.metadata["openapi-docs"]
+//                println("SERVICE: $serviceName, OPENAPI URL: $openApiUrl")
+//                if (!openApiUrl.isNullOrBlank()) {
+//                    swaggerUiConfigParameters.addGroup(serviceName, openApiUrl)
+//                }
+//            }
+//        }
+//    }
 }
