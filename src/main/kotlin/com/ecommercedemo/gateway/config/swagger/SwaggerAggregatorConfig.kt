21754ca10fbc97b2516ca2a11e62bcc00a7876d4
aron <vaupelaron@gmail.com>
2025-01-20 22:13:59 +0700

    re-introduce aggregation and add url

--- src/main/kotlin/com/ecommercedemo/gateway/config/swagger/SwaggerAggregatorConfig.kt
+++ src/main/kotlin/com/ecommercedemo/gateway/config/swagger/SwaggerAggregatorConfig.kt
@@ -1,8 +1,8 @@
 package com.ecommercedemo.gateway.config.swagger
 
-import jakarta.annotation.PostConstruct
 import org.springdoc.core.properties.SwaggerUiConfigParameters
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.Scheduled
 
 @Configuration
 class SwaggerAggregatorConfig(
@@ -10,20 +10,21 @@ class SwaggerAggregatorConfig(
     private val discoveryClient: org.springframework.cloud.client.discovery.DiscoveryClient
 ) {
 
-//    @PostConstruct
-//    fun configureSwagger() {
-//        val services = discoveryClient.services
-//        println("SWAGGER DISCOVERED SERVICES: $services")
-//        services.forEach { serviceName ->
-//            val instances = discoveryClient.getInstances(serviceName)
-//            println("SWAGGER DISCOVERED INSTANCES: $serviceName, INSTANCES: $instances")
-//            instances.forEach { instance ->
-//                val openApiUrl = instance.metadata["openapi-docs"]
-//                println("SERVICE: $serviceName, OPENAPI URL: $openApiUrl")
-//                if (!openApiUrl.isNullOrBlank()) {
-//                    swaggerUiConfigParameters.addGroup(serviceName, openApiUrl)
-//                }
-//            }
-//        }
-//    }
+    @Scheduled(fixedRate = 30000)
+    fun configureSwagger() {
+        val services = discoveryClient.services
+        println("SWAGGER DISCOVERED SERVICES: $services")
+        services.forEach { serviceName ->
+            val instances = discoveryClient.getInstances(serviceName)
+            println("SWAGGER DISCOVERED INSTANCES: $serviceName, INSTANCES: $instances")
+            instances.forEach { instance ->
+                val openApiUrl = instance.metadata["openapi-docs"]
+                println("SERVICE: $serviceName, OPENAPI URL: $openApiUrl")
+                if (!openApiUrl.isNullOrBlank()) {
+                    swaggerUiConfigParameters.addGroup(serviceName)
+                    swaggerUiConfigParameters.addUrl(openApiUrl)
+                }
+            }
+        }
+    }
 }
